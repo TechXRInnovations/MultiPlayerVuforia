b4ce0f359d4e75a86ba778c9b464d6b526d9983d
Shashank <mishtashashank31@gmail.com>
2022-02-12 11:47:42 +0530

    first commit

---
+++ Assets/Scripts/CreateAndJoin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CreateAndJoin : MonoBehaviourPunCallbacks
+{
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom("Test");
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom("Test");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("03-Game");
+    }
+}
---
+++ Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = new Vector3(Random.Range(0, 3), 0,Random.Range(0, 3));
+        PhotonNetwork.Instantiate(PlayerPrefab.name, position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
